4f978eee083ce27c0b8c97085bb6509f30766f07
devadg <opponamer@gmail.com>
2024-10-26 07:42:03 +0100

    done

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import { HiOutlineBars3BottomRight } from "react-icons/hi2";
 
 export default function Navbar() {
     return (
-        <div className="w-full fixed top-0 z-50 py-2 mix-blend-difference">
+        <div className="w-full fixed top-0 z-50 py-2 bg-black/10 backdrop-blur-sm">
             <div className="w-11/12 mx-auto flex justify-between items-center uppercase font-medium">
                 <h1 className="font-medium hidden md:block">Great Alexander (ADG).</h1>
                 <h1 className="font-light text-2xl md:hidden block">ADG</h1>
--- app/page.js
+++ app/page.js
@@ -15,6 +15,8 @@ import { TbApi } from "react-icons/tb";
 import { LuDot } from "react-icons/lu";
 import { useState } from "react";
 
+import apexIcon from '@/public/apex.webp'
+
 const data = [
   {id: 1, title: 'Why work with me ?', content: 'In today’s fast-paced world, flexibility is essential. I offer high-quality design and web development expertise on a flexible basis, so you get the skills you need without the commitment of full-time hiring. Whether it’s a short-term project or ongoing support, pay only for what you need, when you need it. This approach means you’re not tied down to unnecessary costs, and you get tailored service with every project.'},
   {id: 2, title: 'Are there any refunds if i dont like the services?', content: 'Unfortunately we do not provide any refunds. This is due to the time and effort invested into producing high-quality design and development work. We can either continue improving the development until you are satisfied or you can pause/cancel your subscription.'},
@@ -58,7 +60,7 @@ export default function Home() {
               </h1>
               <div className="w-full flex gap-3 mt-5 md:mt-10 uppercase">
                 <Link href={'#'} className="bg-green-500 text-white py-1.5 px-6 rounded-md border-2 border-green-800">About</Link>
-                <Link href={'#'} className="bg-green-500 text-white py-1.5 px-6 rounded-md border-2 border-green-800">Contact Me</Link>
+                <Link href={'#'} className="bg-green-500 text-white py-1.5 px-6 rounded-md border-2 border-green-800">Let&rsquo;s Work</Link>
               </div>
           </div>
           <div className="w-full md:w-5/12 flex justify-center">
@@ -143,16 +145,140 @@ export default function Home() {
             <div className="border py-3 border-green-500"></div>
             <h1 className="mt-0.5 font-medium uppercase">About the Mind Behind the Code</h1>
           </div>
-          <div className="mt-5 w-full flex">
+          <div className="mt-5 w-full flex flex-col md:flex-row gap-12 md:gap-3">
             <div className="w-full md:w-6/12">
-              <div className="text-lg">
                 <p>I’m a Fullstack Developer with 4 years of experience creating high-performance, user-centered web applications. Specializing in Next.js, React.js, Node.js, and MongoDB, I blend technical expertise with a passion for sleek, functional design.</p>
                 <br/>
-                <p>Using tools like Framer Motion, GSAP, and Tailwind CSS, I deliver smooth animations and responsive, visually engaging interfaces that elevate brands and engage users.</p>
-                <Link href={'#'} className="text-green-800 underline hover:text-green-700">Let’s create something exceptional together!</Link>
+                <p className="mb-5">Using tools like Framer Motion, GSAP, and Tailwind CSS, I deliver smooth animations and responsive, visually engaging interfaces that elevate brands and engage users.</p>
+                <div className="w-full flex gap-2 items-center mb-3">
+                  <div className="border py-3 border-green-500"></div>
+                  <h1 className="mt-0.5 font-medium uppercase">My Process</h1>
+                </div>
+                <ul class="relative flex flex-row gap-x-2 mb-8">
+                  <li class="flex items-center gap-x-2 shrink basis-0 flex-1 group">
+                    <div class="min-w-7 min-h-7 inline-flex justify-center items-center text-xs align-middle">
+                      <span class="size-7 flex justify-center items-center shrink-0 bg-gray-100 font-medium text-gray-800 rounded-full dark:bg-green-800 dark:text-white">
+                        1
+                      </span>
+                      <span class="ms-2 block text-sm font-medium text-gray-800 dark:text-white">
+                        Design
+                      </span>
+                    </div>
+                    <div class="w-full h-px flex-1 bg-gray-200 group-last:hidden dark:bg-neutral-700"></div>
+                  </li>
+                  <li class="flex items-center gap-x-2 shrink basis-0 flex-1 group">
+                    <div class="min-w-7 min-h-7 inline-flex justify-center items-center text-xs align-middle">
+                      <span class="size-7 flex justify-center items-center shrink-0 bg-gray-100 font-medium text-gray-800 rounded-full dark:bg-green-800 dark:text-white">
+                        2
+                      </span>
+                      <span class="ms-2 block text-sm font-medium text-gray-800 dark:text-white">
+                        Development
+                      </span>
+                    </div>
+                    <div class="w-full h-px flex-1 bg-gray-200 group-last:hidden dark:bg-neutral-700"></div>
+                  </li>
+                  <li class="flex items-center gap-x-2 shrink basis-0 flex-1 group">
+                    <div class="min-w-7 min-h-7 inline-flex justify-center items-center text-xs align-middle">
+                      <span class="size-7 flex justify-center items-center shrink-0 bg-gray-100 font-medium text-gray-800 rounded-full dark:bg-green-800 dark:text-white">
+                        3
+                      </span>
+                      <span class="ms-2 block text-sm font-medium text-gray-800 dark:text-white">
+                        Launch & Support
+                      </span>
+                    </div>
+                    <div class="w-full h-px flex-1 bg-gray-200 group-last:hidden dark:bg-neutral-700"></div>
+                  </li>
+                </ul>
+                <div className="w-full flex gap-2 items-center mb-3">
+                  <div className="border py-3 border-green-500"></div>
+                  <h1 className="mt-0.5 font-medium uppercase">My Stack / tools</h1>
+                </div>
+                <div className="flex flex-wrap gap-3 mb-6">
+                  <FaHtml5 className="h-10 w-10 text-gray-200" />
+                  <FaCss3Alt className="h-10 w-10 text-gray-200" />
+                  <RiTailwindCssFill className="h-10 w-10 text-gray-200" />
+                  <RiNextjsFill className="h-10 w-10 text-gray-200" />
+                  <FaReact className="h-10 w-10 text-gray-200" />
+                  <FaNodeJs className="h-10 w-10 text-gray-200" />
+                  <SiMongodb className="h-10 w-10 text-gray-200" />
+                  <SiExpress className="h-10 w-10 text-gray-200" />
+                  <FaPhp className="h-10 w-10 text-gray-200" />
+                  <TbApi className="h-10 w-10 text-gray-200" />
+                  <FaGitAlt className="h-10 w-10 text-gray-200" />
+                  <FaGithub className="h-10 w-10 text-gray-200" />
+                  <SiPostman className="h-10 w-10 text-gray-200" />
+                </div>
+                <Link href={'#'} className="rounded-md py-1.5 px-6 border-green-800 border-2 bg-green-500 duration-500 hover:bg-green-600 uppercase">Let&rsquo;s Work</Link>
+            </div>
+            <div className="w-full md:w-6/12">
+              <div className="w-full flex gap-2 items-center mb-5">
+                <div className="border py-3 border-green-500"></div>
+                <h1 className="mt-0.5 font-medium uppercase">Work Experience</h1>
+              </div>
+              {/* treeline */}
+              <div>
+                <div class="group relative flex gap-x-5">
+                  <div class="relative group-last:after:hidden after:absolute after:top-8 after:bottom-2 after:start-3 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 dark:after:bg-neutral-700">
+                    <div class="relative z-10 size-6 flex justify-center items-center">
+                      <svg class="shrink-0 size-6 text-gray-800 dark:text-neutral-200" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M12 12h.01"/><path d="M16 6V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v2"/><path d="M22 13a18.15 18.15 0 0 1-20 0"/><rect width="20" height="14" x="2" y="6" rx="2"/></svg>
+                    </div>
+                  </div>
+                  <div class="grow pb-8 group-last:pb-0">
+                    <h3 class="mb-1 text-xs text-gray-500">
+                      10/2022 - Present
+                    </h3>
+
+                    <p class="font-semibold text-sm text-gray-800 dark:text-neutral-200">
+                      Freelance Fullstack Web Designer & Web Developer
+                    </p>
+
+                    <p class="mt-1 text-sm text-gray-500">
+                      Worked with a diverse range of clients, delivering tailored design solutions, developed and maintained strong client relationships through effective communication and project management.
+                    </p>
+                  </div>
+                </div>
+                <div class="group relative flex gap-x-5">
+                  <div class="relative group-last:after:hidden after:absolute after:top-8 after:bottom-2 after:start-3 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 dark:after:bg-neutral-700">
+                    <div class="relative z-10 size-6 flex justify-center items-center">
+                      <Image src={apexIcon} className="" alt="" />
+                    </div>
+                  </div>
+                  <div class="grow pb-8 group-last:pb-0">
+                    <h3 class="mb-1 text-xs text-gray-500">
+                      02/2022 - 10/2022
+                    </h3>
+
+                    <p class="font-semibold text-sm text-gray-800 dark:text-neutral-200">
+                      Fullstack Software Developer at Apexloaded Ltd.
+                    </p>
+
+                    <p class="mt-1 text-sm text-gray-500">
+                      Apexloaded Global Limited is a technology company solving problems by developing custom software based solutions to meet individual and business needs.
+                    </p>
+                  </div>
+                </div>
+                <div class="group relative flex gap-x-5">
+                  <div class="relative group-last:after:hidden after:absolute after:top-8 after:bottom-2 after:start-3 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 dark:after:bg-neutral-700">
+                    <div class="relative z-10 size-6 flex justify-center items-center">
+                      <Image src={apexIcon} className="" alt="" />
+                    </div>
+                  </div>
+                  <div class="grow pb-8 group-last:pb-0">
+                    <h3 class="mb-1 text-xs text-gray-500">
+                      09/2021 - 02/2022
+                    </h3>
+
+                    <p class="font-semibold text-sm text-gray-800 dark:text-neutral-200">
+                      Intern Frontend Developer at Apexloaded
+                    </p>
+
+                    <p class="mt-1 text-sm text-gray-500">
+                      I was awarded the Best Junior Frontend Developer in Apexloaded🏆.
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="w-full md:w-6/12"></div>
           </div>
         </div>
       </section>
@@ -243,10 +369,9 @@ export default function Home() {
               <h1 className="text-3xl uppercase">Need help with a project ?</h1>
               <span className="text-gray-500">Feel free to reach out if you&rsquo;d like to collaborate, need help with a project, or just want to connect. I&rsquo;m open to new opportunities and ideas.</span>
               <div className="w-full mt-5">
-                <Link href={'#'} className="bg-green-500 rounded-md py-1.5 px-6 uppercase border-2 border-green-800 animate-pulse">Say Hello</Link>
+                <Link href={'#'} className="bg-green-500 rounded-md py-1.5 px-6 uppercase border-2 border-green-800 duration-500 hover:bg-green-600">Let&rsquo;s Work</Link>
               </div>
             </div>
-            
           </div>
         </div>
       </section>
